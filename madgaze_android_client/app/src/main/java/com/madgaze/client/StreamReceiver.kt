fcb1b06e3737171e283f9193bc2fd042ce4f4868
lil-fahad <73719703+lil-fahad@users.noreply.github.com>
2025-05-04 22:56:11 +0300

    Delete madgaze_android_client directory

--- madgaze_android_client/app/src/main/java/com/madgaze/client/StreamReceiver.kt
+++
@@ -1,9 +0,0 @@
-package com.madgaze.client
-
-import android.view.SurfaceView
-
-class StreamReceiver(val surfaceView: SurfaceView) {
-    fun startStreaming(url: String) {
-        // Start decoding incoming video stream
-    }
-}
